a2baa80f3b73eb11f71f12224fee224b76d53539
佐内豊 <yutaka.0371@gmail.com>
2020-10-09 17:50:11 +0900

    変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Routing\UrlGenerator;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -23,12 +24,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         Schema::defaultStringLength(191);
 
-        if (request()->isSecure()) {
-            \URL::forceScheme('https');
+        $url->forceScheme('https');
         }
     }
-}
+
