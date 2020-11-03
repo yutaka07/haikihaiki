403287abdb25f35a41506075ede73cfaecc82e87
佐内豊 <yutaka.0371@gmail.com>
2020-11-03 15:34:41 +0900

    変更

--- resources/views/layouts/user/app.blade.php
+++ resources/views/layouts/user/app.blade.php
@@ -99,7 +99,7 @@
 
          <!-- フラッシュメッセージ -->
          @if (session('flash_message'))
-                <div class="c-alert">
+                <div class="alert alert-success">
                     {{ session('flash_message') }}
                 </div>
         @endif
