399ce8576bddd690d62407f5d293f3574958070a
佐内豊 <yutaka.0371@gmail.com>
2020-11-19 23:49:39 +0900

    変更

--- resources/views/admin/products/registproduct.blade.php
+++ resources/views/admin/products/registproduct.blade.php
@@ -14,86 +14,84 @@
 
                         <div class="c-form__group row">
                             <div class="col-md-6">
-                               <label for="name">商品名</label>
+                                <label for="name">商品名</label>
                                 <input placeholder="商品名" id="name" type="text" class="c-form__group--control @error('name') c-form__error--background @enderror" name="name" value="{{ old('name') }}" maxlength='190' required autocomplete="name" autofocus>
 
                                 @error('name')
-                                    <span  role="alert">
-                                        <strong class="c-form__error--text">{{ $message }}</strong>
-                                    </span>
+                                <span role="alert">
+                                    <strong class="c-form__error--text">{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
                         <div class="c-form__group row">
-                           
+
 
                             <div class="col-md-6">
-                              <label for="expiration_date">消費期限</label>
-                                <input  id="expiration_date" type="datetime-local" class="c-form__group--control @error('expiration_date') c-form__error--background @enderror" name="expiration_date" value="{{ $date }}" required autocomplete="expiration_date" autofocus>
+                                <label for="expiration_date">消費期限</label>
+                                <input id="expiration_date" type="datetime-local" class="c-form__group--control @error('expiration_date') c-form__error--background @enderror" name="expiration_date" value="{{ $date }}" required autocomplete="expiration_date" autofocus>
 
                                 @error('expiration_date')
-                                    <span  role="alert">
-                                        <strong class="c-form__error--text">{{ $message }}</strong>
-                                    </span>
+                                <span role="alert">
+                                    <strong class="c-form__error--text">{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
                         <div class="c-form__group row">
-                           
+
 
                             <div class="col-md-6">
-                            <label for="price">価格（円）</label>
-                                <input placeholder="999,999以下"  id="price" type="number" pattern="\d*" min="0" max="999999" class="c-form__group--control @error('price') c-form__error--background @enderror" name="price" value="{{ old('price') }}"  required autocomplete="price" autofocus>
+                                <label for="price">価格（円）</label>
+                                <input placeholder="999,999以下" id="price" type="number" pattern="\d*" min="0" max="999999" class="c-form__group--control @error('price') c-form__error--background @enderror" name="price" value="{{ old('price') }}" required autocomplete="price" autofocus>
 
                                 @error('price')
-                                    <span  role="alert">
-                                        <strong class="c-form__error--text">{{ $message }}</strong>
-                                    </span>
+                                <span role="alert">
+                                    <strong class="c-form__error--text">{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
                         <div class="c-form__group row">
-                           
+
 
                             <div class="col-md-6">
-                            <label for="comment">商品詳細(190文字以下)</label>
-                                <textarea cols="30" rows="10" id="comment" type="comment" class="c-form__group--textarea @error('comment') c-form__error--background @enderror" name="comment"  maxlength='190' required autocomplete="comment">
-                                {{ old('comment') }}
-                                </textarea>
+                                <label for="comment">商品詳細(190文字以下)</label>
+                                <textarea cols="30" rows="10" id="comment" type="comment" class="c-form__group--textarea @error('comment') c-form__error--background @enderror" name="comment" maxlength='190' required autocomplete="comment">{{ old('comment') }}</textarea>
 
                                 @error('comment')
-                                    <span  role="alert">
-                                        <strong class="c-form__error--text">{{ $message }}</strong>
-                                    </span>
+                                <span role="alert">
+                                    <strong class="c-form__error--text">{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
                         <div class="c-form__group row">
-                            
+
                             <div class="col-md-6">
                                 <label class="c-form__group--photofilelabel" for="photofile">写真を選択</label>
-                                <input placeholder="ファイル" accept="image/*" id="photofile" type="file" class="c-form__group--control c-form__group--photofile @error('photofile') c-form__error--background @enderror" name="photofile"  autocomplete="new-photofile">
+                                <input placeholder="ファイル" accept="image/*" id="photofile" type="file" class="c-form__group--control c-form__group--photofile @error('photofile') c-form__error--background @enderror" name="photofile" autocomplete="new-photofile">
 
                                 @error('photofile')
-                                    <span  role="alert">
-                                        <strong class="c-form__error--text">{{ $message }}</strong>
-                                    </span>
+                                <span role="alert">
+                                    <strong class="c-form__error--text">{{ $message }}</strong>
+                                </span>
                                 @enderror
-                                <img class="c-form__group--img" id="preview"  alt="" />
+                                <img class="c-form__group--img" id="preview" alt="" />
                             </div>
                         </div>
 
-                        
+
 
                         <div class="c-form__group row mb-0">
-                           
-                                <button type="submit" class="c-btn__form">
-                                    出品
-                                </button>
-                           
+
+                            <button type="submit" class="c-btn__form">
+                                出品
+                            </button>
+
                         </div>
                     </form>
                 </div>
